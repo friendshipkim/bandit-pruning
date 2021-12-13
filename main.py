81dcc57a2f1b31b4c47a002417b1bf5b15e96d7a
friendshipkim <kwj962004@gmail.com>
2021-12-12 22:41:03 -0500

    branch main.py

--- main.py
+++ main.py
@@ -96,6 +96,14 @@ def main():
         type=float,
         help="Threshold of accuracy drop",
     )
+
+    parser.add_argument(
+        "--block_num",
+        default=None,
+        type=int,
+        help="Which transformer block to prune",
+    )
+
     parser.add_argument("--seed", type=int, default=2021)
 
     args = parser.parse_args()
@@ -107,6 +115,12 @@ def main():
         raise Exception("Invalid algo name")
     if args.thres is None and args.pruning_type is not 'head_whole_nothres':
         raise Exception("'thres' necessary for this pruning type")
+    if args.block_num is None and args.pruning_type in ['head', 'neuron']:
+        raise Exception("'block_num' necessary for this pruning type")
+
+    pruning_type = 'head' if args.pruning_type == 'head' \
+        else 'headwhole' if 'headwhole' in args.pruning_type \
+        else 'neuron'
 
     # load dataset
     encoded_dataset = load_from_disk(args.data_dir)
@@ -146,20 +160,24 @@ def main():
 
     # model configs
     n_layers, n_heads, d_model = model.config.num_hidden_layers, model.config.num_attention_heads, model.config.hidden_size
+    hidden_dim = model.config.hidden_dim
     d_k = d_model // n_heads
     print('n_layers:', n_layers)
     print('n_heads:', n_heads)
     print('d_model:', d_model)
+    print('hidden_dim:', hidden_dim)
     print('d_k:', d_k)
 
     # bandit configs
-    n_arms = n_heads if args.pruning_type == 'head' else n_layers * n_heads if 'headwhole' in args.pruning_type else None
-    arms = np.arange(n_arms)
+    n_arms = n_heads if pruning_type == 'head' \
+        else n_layers * n_heads if pruning_type == 'headwhole' \
+        else hidden_dim
 
     # hyperparameters
     rounds = args.rounds  # Playing times
     thres = args.thres # maximum reward
     algo_name = args.algo_name
+    block_num = args.block_num
 
     # choose algo
     algo = algo_dict[algo_name]
@@ -168,89 +186,143 @@ def main():
     start_time = time.time()
 
     # bandit playing
-    for t in tqdm(range(rounds)):
-        print('============ {}th iteration =============='.format(t))
+
+    if args.algo_name == 'random':
+        prune_order = np.arange(n_arms)
+        np.random.shuffle(prune_order)
+
+        print('Start random pruning')
         model = copy.deepcopy(model_copy)  # restore full model
-        chosen_arm = algo.select_arm()  # select arm (head) to prune
+        prune_after_accuracy = np.zeros(n_arms)
 
-        # whole layers
-        block_num = chosen_arm // n_heads
-        head_num = chosen_arm % n_heads
-        print('chosen_arm: {}th block {}th head'.format(block_num, head_num))
+        for t in tqdm(range(n_arms)):  # for whole heads
+            prune_idx = prune_order[t]  # x=np.argmax(rewards)
+            if pruning_type == 'head':
+                # prune head
+                print('Remaining heads: {} -- Now prune {}th head'.format(n_heads - t - 1, prune_idx))
+                zeroize_head_weights(model, block_num, prune_idx)
 
-        # prune head
-        zeroize_head_weights(model, block_num, head_num)
-        pruned_loss, pruned_accuracy = test_pruned(model, trainer, metric_name, prng)
-        print('pruned_accuracy:', pruned_accuracy)
-        print('pruned_loss:', pruned_loss)
-        print()
+            elif pruning_type == 'headwhole':  # whole layers
+                block_num = prune_idx // n_heads
+                head_num = prune_idx % n_heads
+                print('Now prune {}th block {}th head -- Remaining heads: {}'.format(block_num, head_num, n_arms - t - 1))
+                zeroize_head_weights(model, block_num, head_num)
 
-        delta_accuracy = pruned_accuracy - full_accuracy
-        delta_loss = pruned_loss - full_mean_loss
-        print("delta_accuracy:", delta_accuracy)
-        print("delta_loss:", delta_loss)
+            else:
+                print('Remaining neurons: {} -- Now prune {}th neuron'.format(n_arms - t - 1, prune_idx))
+                zeroize_neuron_weights(model, block_num, prune_idx)
 
-        # ver1 prune by accuracy
-        """
-        if accuracy increases after pruning (delta > 0), reward = min(thres + delta
-        if accuracy decreases after pruning (delta < 0), reward = max(0, thres + delta)
-        """
-        if args.pruning_type == "headwhole_nothres":
-            reward = delta_accuracy
-        else:
-            reward = max(0, thres + delta_accuracy)
-        print("reward:", reward)
+            pruned_loss, pruned_accuracy = test_full(model, trainer, metric_name)
 
-        algo.update(chosen_arm, reward)
-        print()
+            prune_after_accuracy[t] = pruned_accuracy
+            print("Accuracy after pruning = {:.2f}".format(pruned_accuracy * 100))
+            print("Remaining parameters = {:.2f}%".format(count_nonzero_weights(model) * 100 / full_weights_count))
+            print()
 
-    # measure playing time
-    print("The time for running {} algorithm is {} seconds ".format(algo_name, time.time() - start_time))
+    else: # MAB
+        print("===========MAB Algorithm : {}===========".format(algo_name))
 
-    # pruning
-    print('Start pruning')
-    expected_rewards = copy.deepcopy(algo.weights) if algo_name == 'Exp3' else copy.deepcopy(algo.values)
-    model = copy.deepcopy(model_copy)  # restore full model
+        for t in tqdm(range(rounds)):
+            print('============ {}th iteration =============='.format(t))
+            model = copy.deepcopy(model_copy)  # restore full model
+            chosen_arm = algo.select_arm()  # select arm (head) to prune
 
-    prune_order = []
-    prune_after_accuracy = np.zeros(n_arms)  # AccuracyAftrerPrune
+            if pruning_type == 'head':
+                # prune head
+                zeroize_head_weights(model, block_num, chosen_arm)
 
-    for t in tqdm(range(n_arms)):  # for whole heads
-        prune_idx = np.argmax(expected_rewards)
-        prune_order.append(prune_idx)
+            elif pruning_type == 'headwhole': # whole layers
+                block_num = chosen_arm // n_heads
+                head_num = chosen_arm % n_heads
+                print('chosen_arm: {}th block {}th head'.format(block_num, head_num))
+                # prune head
+                zeroize_head_weights(model, block_num, head_num)
 
-        block_num = prune_idx // n_heads
-        head_num = prune_idx % n_heads
-        print('Now prune {}th block {}th head -- Remaining heads: {}'.format(block_num, head_num, n_arms - t - 1))
+            else:
+                zeroize_neuron_weights(model, block_num, chosen_arm)
 
-        expected_rewards[prune_idx] = -100
+            pruned_loss, pruned_accuracy = test_pruned(model, trainer, metric_name, prng)
+            print('pruned_accuracy:', pruned_accuracy)
+            print('pruned_loss:', pruned_loss)
+            print()
 
-        zeroize_head_weights(model, block_num, head_num)
-        pruned_loss, pruned_accuracy = test_full(model, trainer, metric_name)
+            delta_accuracy = pruned_accuracy - full_accuracy
+            delta_loss = pruned_loss - full_mean_loss
+            print("delta_accuracy:", delta_accuracy)
+            print("delta_loss:", delta_loss)
 
-        prune_after_accuracy[t] = pruned_accuracy
-        print("Accuracy after pruning = {:.2f}".format(pruned_accuracy * 100))
-        print("Remaining parameters = {:.2f}%".format(count_nonzero_weights(model) * 100 / full_weights_count))
-        print()
+            # ver1 prune by accuracy
+            """
+            if accuracy increases after pruning (delta > 0), reward = min(thres + delta
+            if accuracy decreases after pruning (delta < 0), reward = max(0, thres + delta)
+            """
+            if args.pruning_type == "headwhole_nothres":
+                reward = delta_accuracy
+            else:
+                reward = max(0, thres + delta_accuracy)
+            print("reward:", reward)
+
+            algo.update(chosen_arm, reward)
+            print()
+
+        # measure playing time
+        print("The time for running {} algorithm is {} seconds ".format(algo_name, time.time() - start_time))
+
+        # pruning
+        print('Start pruning')
+        expected_rewards = copy.deepcopy(algo.weights) if algo_name == 'Exp3' else copy.deepcopy(algo.values)
+        model = copy.deepcopy(model_copy)  # restore full model
+
+        prune_order = []
+        prune_after_accuracy = np.zeros(n_arms)
+
+        for t in tqdm(range(n_arms)):  # for whole heads
+            prune_idx = np.argmax(expected_rewards)
+            prune_order.append(prune_idx)
+
+            expected_rewards[prune_idx] = -100
+
+            if pruning_type == 'head':
+                # prune head
+                print('Remaining heads: {} -- Now prune {}th head'.format(n_heads - t - 1, prune_idx))
+                zeroize_head_weights(model, block_num, prune_idx)
+
+            elif pruning_type == 'headwhole':  # whole layers
+                block_num = prune_idx // n_heads
+                head_num = prune_idx % n_heads
+                print('Now prune {}th block {}th head -- Remaining heads: {}'.format(block_num, head_num, n_arms - t - 1))
+                zeroize_head_weights(model, block_num, head_num)
+
+            else:
+                print('Remaining neurons: {} -- Now prune {}th neuron'.format(n_arms - t - 1, prune_idx))
+                zeroize_neuron_weights(model, block_num, prune_idx)
+
+            pruned_loss, pruned_accuracy = test_full(model, trainer, metric_name)
+
+            prune_after_accuracy[t] = pruned_accuracy
+            print("Accuracy after pruning = {:.2f}".format(pruned_accuracy * 100))
+            print("Remaining parameters = {:.2f}%".format(count_nonzero_weights(model) * 100 / full_weights_count))
+            print()
 
     # save data
-    result_path = os.path.join('./results', args.pruning_type, algo_name)
+    result_path = os.path.join('./results', args.pruning_type, algo_name) if pruning_type == 'headwhole' \
+        else os.path.join('./results', pruning_type, str(block_num), algo_name)
     print('result_path:', result_path)
 
     # Create a new directory if it does not exist
-    # if not os.path.exists(result_path):
-    #     os.makedirs(result_path)
-    #     print("The new directory is created!")
-    #
-    # if algo_name == 'Exp3':
-    #     np.save(os.path.join(result_path, "expected_rewards.npy"), algo.weights)
-    #     np.save(os.path.join(result_path, "prune_order.npy"), prune_order)
-    #     np.save(os.path.join(result_path, "prune_after_accuracy.npy"), prune_after_accuracy)
-    # else:
-    #     np.save(os.path.join(result_path, "counts.npy"), algo.counts)
-    #     np.save(os.path.join(result_path, "expected_rewards.npy"), algo.values)
-    #     np.save(os.path.join(result_path, "prune_order.npy"), prune_order)
-    #     np.save(os.path.join(result_path, "prune_after_accuracy.npy"), prune_after_accuracy)
+    if not os.path.exists(result_path):
+        os.makedirs(result_path)
+        print("The new directory is created!")
+
+    if algo_name == 'Exp3':
+        np.save(os.path.join(result_path, "expected_rewards.npy"), algo.weights)
+        np.save(os.path.join(result_path, "prune_order.npy"), prune_order)
+        np.save(os.path.join(result_path, "prune_after_accuracy.npy"), prune_after_accuracy)
+    else:
+        np.save(os.path.join(result_path, "counts.npy"), algo.counts)
+        np.save(os.path.join(result_path, "expected_rewards.npy"), algo.values)
+        np.save(os.path.join(result_path, "prune_order.npy"), prune_order)
+        np.save(os.path.join(result_path, "prune_after_accuracy.npy"), prune_after_accuracy)
 
 if __name__ == "__main__":
     main()
